d5a7625e310e080847527e9847165faed323f23a
Doritos555 <joaopedrotavaresdeassis57@gmail.com>
2025-09-26 17:13:22 -0300

    Update powers.js

--- powers.js
+++ powers.js
@@ -18,42 +18,42 @@ const riders = {
     "Xross Saber": "imagens/Saber/Xross Saber.jpg"
   },
   revice: {
-    "Rex Genome": "imagens/revice/base.jpg",
-    "Barid Rex Genome": "imagens/revice/Barid Rex Genome.jpg",
-    "Volcano Rex Genome/Barid Rex Genome": "imagens/revice/Volcano Rex Genome.jpg",
-    "Jack Revice Genome": "imagens/revice/Jack Revice Genome.jpg",
-    "Thunder Gale Genome": "imagens/revice/Thunder Gale Genome.jpg",
-    "Ultimate Genome Revi/Ultimate Genome Vice": "imagens/revice/Ultimate Genome.jpg"
+    "Rex Genome": "Imagens/Revice/base.jpg",
+    "Barid Rex Genome": "Imagens/Revice/Barid Rex Genome.jpg",
+    "Volcano Rex Genome/Barid Rex Genome": "Imagens/Revice/Volcano Rex Genome.jpg",
+    "Jack Revice Genome": "Imagens/Revice/Jack Revice Genome.jpg",
+    "Thunder Gale Genome": "Imagens/Revice/Thunder Gale Genome.jpg",
+    "Ultimate Genome Revi/Ultimate Genome Vice": "Imagens/Revice/Ultimate Genome.jpg"
   },
   geats: {
-    MagnumBoost: "imagens/geats/Base.jpg",
-    "Fever Magnum Form": "imagens/geats/Fever Magnum Form.jpg",
-    "Fever Boost Form": "imagens/geats/Fever Boost Form.jpg",
-    "Command Form Cannon Mode": "imagens/geats/Command Form Cannon Mode.jpg",
-    "Command Form Jet Mode": "imagens/geats/Command Form Jet Mode.jpg",
-    "Boost MarkII": "imagens/geats/Boost MarkII.jpg",
-    "Laser Boost": "imagens/geats/laser boost.jpg",
-    "Boost MarkIII": "imagens/geats/Boost MarkIII.jpg",
-    "Geats MarkIX": "imagens/geats/Geats MarkIX.jpg"
+    MagnumBoost: "Imagens/Geats/Base.jpg",
+    "Fever Magnum Form": "Imagens/Geats/Fever Magnum Form.jpg",
+    "Fever Boost Form": "Imagens/Geats/Fever Boost Form.jpg",
+    "Command Form Cannon Mode": "Imagens/Geats/Command Form Cannon Mode.jpg",
+    "Command Form Jet Mode": "Imagens/Geats/Command Form Jet Mode.jpg",
+    "Boost MarkII": "Imagens/Geats/Boost MarkII.jpg",
+    "Laser Boost": "Imagens/Geats/laser boost.jpg",
+    "Boost MarkIII": "Imagens/Geats/Boost MarkIII.jpg",
+    "Geats MarkIX": "Imagens/Geats/Geats MarkIX.jpg"
   },
   gotchard: {
-    Steamhopper: "imagens/gotchard/base.jpg",
-    "Cross Ufo-X": "imagens/gotchard/Cross Ufo-X.jpg",
-    "Cross X-Rex": "imagens/gotchard/Cross X-Rex.jpg",
-    "Fire Gotchard": "imagens/gotchard/Fire Gotchard.jpg",
-    "Iron Gotchard": "imagens/gotchard/Iron Gotchard.jpg",
-    "Platinum Gotchard": "imagens/gotchard/Platinum Gotchard.jpg",
-    "Rainbow Gotchard": "imagens/gotchard/Rainbow Gotchard.jpg"
+    Steamhopper: "Imagens/Gotchard/Base.jpg",
+    "Cross Ufo-X": "Imagens/Gotchard/Cross Ufo-X.jpg",
+    "Cross X-Rex": "Imagens/Gotchard/Cross X-Rex.jpg",
+    "Fire Gotchard": "Imagens/Gotchard/Fire Gotchard.jpg",
+    "Iron Gotchard": "Imagens/Gotchard/Iron Gotchard.jpg",
+    "Platinum Gotchard": "Imagens/Gotchard/Platinum Gotchard.jpg",
+    "Rainbow Gotchard": "Imagens/Gotchard/Rainbow Gotchard.jpg"
   },
   gavv: {
-    "Poppin Gummy": "imagens/gavv/base.jpg",
-    Caking: "imagens/gavv/caking.jpg",
-    "Blizzard Sorbet": "imagens/gavv/blizzard sorbet.jpg",
-    "Over Gavv": "imagens/gavv/over.jpg",
-    "Master Gavv": "imagens/gavv/master.jpg"
+    "Poppin Gummy": "Imagens/Gavv/Base.jpg",
+    Caking: "Imagens/Gavv/Caking.jpg",
+    "Blizzard Sorbet": "Imagens/Gavv/Blizzard Sorbet.jpg",
+    "Over Gavv": "Imagens/Gavv/Over.jpg",
+    "Master Gavv": "Imagens/Gavv/Master.jpg"
   },
   zeztz: {
-    "Physicam Impact": "Imagens/Zeztz/base.jpg",
+    "Physicam Impact": "Imagens/Zeztz/Base.jpg",
     "Technolom Machinery": "Imagens/Zeztz/Technolom Machinery.jpg",
     "Esprim Barrier": "Imagens/Zeztz/Esprim Barrier.jpg",
     "Paradigm Gravity": "Imagens/Zeztz/Paradigm Gravity.jpg"
@@ -63,11 +63,11 @@ const riders = {
 // ===== Vídeos de cada forma =====
 const riderVideos = {
   zeroone: {
-    "Rising Hopper": "Imagens/Zero One/Videos/base.mp4",
-    "Shining Hopper": "Imagens/Zero One/Videos/Shining Hopper.mp4",
-    "Shining Assault Hopper": "Imagens/Zero One/Videos/Shining Assault Hopper.mp4",
-    "MetalCluster Hopper": "Imagens/Zero One/Videos/MetalCluster Hopper.mp4",
-    "Zero Two": "Imagens/Zero One/Videos/Zero Two.mp4"
+    "Rising Hopper": "Imagens/Zero One/videos/base.mp4",
+    "Shining Hopper": "Imagens/Zero One/videos/Shining Hopper.mp4",
+    "Shining Assault Hopper": "Imagens/Zero One/videos/Shining Assault Hopper.mp4",
+    "MetalCluster Hopper": "Imagens/Zero One/videos/MetalCluster Hopper.mp4",
+    "Zero Two": "Imagens/Zero One/videos/Zero Two.mp4"
   },
   saber: {
     "Brave Dragon": "Imagens/Saber/videos/base.mp4",
@@ -78,45 +78,45 @@ const riderVideos = {
     "Xross Saber": "Imagens/Saber/videos/Xross Saber.mp4"
   },
   revice: {
-    "Rex Genome": "imagens/revice/Videos/base.mp4",
-    "Barid Rex Genome": "imagens/revice/Videos/Barid Rex Genome.mp4",
-    "Volcano Rex Genome/Barid Rex Genome": "imagens/revice/Videos/Volcano Rex Genome.mp4",
-    "Jack Revice Genome": "imagens/revice/Videos/Jack Revice Genome.mp4",
-    "Thunder Gale Genome": "imagens/revice/Videos/Thunder Gale Genome.mp4",
-    "Ultimate Genome Revi/Ultimate Genome Vice": "imagens/revice/Videos/Ultimate Genome.mp4"
+    "Rex Genome": "Imagens/Revice/videos/base.mp4",
+    "Barid Rex Genome": "Imagens/Revice/videos/Barid Rex Genome.mp4",
+    "Volcano Rex Genome/Barid Rex Genome": "Imagens/Revice/videos/Volcano Rex Genome.mp4",
+    "Jack Revice Genome": "Imagens/Revice/videos/Jack Revice Genome.mp4",
+    "Thunder Gale Genome": "Imagens/Revice/videos/Thunder Gale Genome.mp4",
+    "Ultimate Genome Revi/Ultimate Genome Vice": "Imagens/Revice/videos/Ultimate Genome.mp4"
   },
   geats: {
-    MagnumBoost: "Imagens/Geats/Videos/base.mp4",
-    "Fever Magnum Form": "Imagens/Geats/Videos/Fever Magnum Form.mp4",
-    "Fever Boost Form": "Imagens/Geats/Videos/Fever Boost Form.mp4",
-    "Command Form Cannon Mode": "Imagens/Geats/Videos/Command Form Cannon Mode.mp4",
-    "Command Form Jet Mode": "Imagens/Geats/Videos/Command Form Jet Mode.mp4",
-    "Boost MarkII": "Imagens/Geats/Videos/Boost MarkII.mp4",
-    "Laser Boost": "Imagens/Geats/Videos/Laser Boost.mp4",
-    "Boost MarkIII": "Imagens/Geats/Videos/Boost MarkIII.mp4",
-    "Geats MarkIX": "Imagens/Geats/Videos/Geats MarkIX.mp4"
+    MagnumBoost: "Imagens/Geats/videos/base.mp4",
+    "Fever Magnum Form": "Imagens/Geats/videos/Fever Magnum Form.mp4",
+    "Fever Boost Form": "Imagens/Geats/videos/Fever Boost Form.mp4",
+    "Command Form Cannon Mode": "Imagens/Geats/videos/Command Form Cannon Mode.mp4",
+    "Command Form Jet Mode": "Imagens/Geats/videos/Command Form Jet Mode.mp4",
+    "Boost MarkII": "Imagens/Geats/videos/Boost MarkII.mp4",
+    "Laser Boost": "Imagens/Geats/videos/Laser Boost.mp4",
+    "Boost MarkIII": "Imagens/Geats/videos/Boost MarkIII.mp4",
+    "Geats MarkIX": "Imagens/Geats/videos/Geats MarkIX.mp4"
   },
   gotchard: {
-    Steamhopper: "Imagens/Gotchard/Videos/base.mp4",
-    "Cross Ufo-X": "Imagens/Gotchard/Videos/Cross Ufo-X.mp4",
-    "Cross X-Rex": "Imagens/Gotchard/Videos/Cross X-Rex.mp4",
-    "Fire Gotchard": "Imagens/Gotchard/Videos/Fire Gotchard.mp4",
-    "Iron Gotchard": "Imagens/Gotchard/Videos/Iron Gotchard.mp4",
-    "Platinum Gotchard": "Imagens/Gotchard/Videos/Platinum Gotchard.mp4",
-    "Rainbow Gotchard": "Imagens/Gotchard/Videos/Rainbow Gotchard.mp4"
+    Steamhopper: "Imagens/Gotchard/videos/base.mp4",
+    "Cross Ufo-X": "Imagens/Gotchard/videos/Cross Ufo-X.mp4",
+    "Cross X-Rex": "Imagens/Gotchard/videos/Cross X-Rex.mp4",
+    "Fire Gotchard": "Imagens/Gotchard/videos/Fire Gotchard.mp4",
+    "Iron Gotchard": "Imagens/Gotchard/videos/Iron Gotchard.mp4",
+    "Platinum Gotchard": "Imagens/Gotchard/videos/Platinum Gotchard.mp4",
+    "Rainbow Gotchard": "Imagens/Gotchard/videos/Rainbow Gotchard.mp4"
   },
   gavv: {
-    "Poppin Gummy": "Imagens/Gavv/Videos/base.mp4",
-    Caking: "Imagens/Gavv/Videos/Caking.mp4",
-    "Blizzard Sorbet": "Imagens/Gavv/Videos/Blizzard Sorbet.mp4",
-    "Over Gavv": "Imagens/Gavv/Videos/Over Gavv.mp4",
-    "Master Gavv": "Imagens/Gavv/Videos/base.mp4"
+    "Poppin Gummy": "Imagens/Gavv/videos/base.mp4",
+    Caking: "Imagens/Gavv/videos/Caking.mp4",
+    "Blizzard Sorbet": "Imagens/Gavv/videos/Blizzard Sorbet.mp4",
+    "Over Gavv": "Imagens/Gavv/videos/Over Gavv.mp4",
+    "Master Gavv": "Imagens/Gavv/videos/base.mp4"
   },
   zeztz: {
     base: "Imagens/Zeztz/Videos/base.mp4",
-    "Technolom Machinery": "Imagens/Zeztz/Videos/Technolom Machinery.mp4",
-    "Esprim Barrier": "Imagens/Zeztz/Videos/Esprim Barrier.mp4",
-    "Paradigm Gravity": "Imagens/Zeztz/Videos/Paradigm Gravity.mp4"
+    "Technolom Machinery": "Imagens/Zeztz/videos/Technolom Machinery.mp4",
+    "Esprim Barrier": "Imagens/Zeztz/videos/Esprim Barrier.mp4",
+    "Paradigm Gravity": "Imagens/Zeztz/videos/Paradigm Gravity.mp4"
   }
 };
 
